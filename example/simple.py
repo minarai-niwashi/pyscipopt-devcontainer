b2c02c81ff107f637d524d1a25a3fd9d1d624d44
minarai-niwashi <keyaki.ann@gmail.com>
2024-10-08 11:02:54 +0000

    simple.pyファイルを更新

--- example/simple.py
+++ example/simple.py
@@ -2,13 +2,20 @@
 
 from pyscipopt import Model
 
-model = Model("Example")  # model name is optional
 
-x = model.addVar("x")
-y = model.addVar("y", vtype="INTEGER")
-model.setObjective(x + y)
-model.addCons(2 * x - y * y >= 0)
-model.optimize()
-sol = model.getBestSol()
-print(f"x: {sol[x]}")
-print(f"y: {sol[y]}")
+def main():
+    model = Model("Example")  # model name is optional
+
+    x = model.addVar("x")
+    y = model.addVar("y", vtype="INTEGER")
+    model.setObjective(x + y)
+    model.addCons(2 * x - y * y >= 0)
+    model.optimize()
+    sol = model.getBestSol()
+    return sol[x], sol[y]
+
+
+if __name__ == "__main__":
+    x, y = main()
+    print(f"x: {x}")
+    print(f"y: {y}")
